d8677e8e2ee7b13b12ad6492e2d5c2465609459a
Maia <matteusmaia889@gmail.com>
2024-08-28 19:49:58 -0300

    feat: home

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,7 +1,48 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen ">
-      <h1>Home</h1>
-    </main>
+    <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-4">
+      <Link
+        href="/"
+        className="group col-span-6 row-span-6 flex justify-center items-end rounded-lg bg-zinc-900 overflow-hidden"
+      >
+        <Image
+          src="/moletom-ai-side.png"
+          className="group-hover:scale-105  transition-transform duration-500"
+          alt="image"
+          height={920}
+          width={920}
+          quality={100}
+        />
+      </Link>
+      <Link
+        href="/"
+        className="group col-span-3 row-span-3 flex justify-center items-end rounded-lg bg-zinc-900 overflow-hidden"
+      >
+        <Image
+          src="/moletom-never-stop-learning.png"
+          className="group-hover:scale-105 transition-transform duration-500"
+          alt="image"
+          height={920}
+          width={920}
+          quality={100}
+        />
+      </Link>
+      <Link
+        href="/"
+        className="group col-span-3 row-span-3 flex justify-center items-end rounded-lg bg-zinc-900 overflow-hidden"
+      >
+        <Image
+          src="/moletom-ia-p-devs.png"
+          className="group-hover:scale-105 transition-transform duration-500"
+          alt="image"
+          height={920}
+          width={920}
+          quality={100}
+        />
+      </Link>
+    </div>
   )
 }

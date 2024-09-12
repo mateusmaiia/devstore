3f829a6b9b5643e7bd95b9156331349bbb5f62f1
Maia <matteusmaia889@gmail.com>
2024-09-11 21:31:46 -0300

    fix

--- src/env.ts
+++ src/env.ts
@@ -1,10 +1,10 @@
 import { z } from 'zod'
 
 const envSchema = z.object({
-  NEXT_PUBLIC_API_BASE_AURL: z.string().url(),
+  NEXT_PUBLIC_API_BASE_URL: z.string().url(),
 })
 
-const parsedEnv = envSchema.safeParse(envSchema)
+const parsedEnv = envSchema.safeParse(process.env)
 
 if (!parsedEnv.success) {
   console.error(
